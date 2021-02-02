c5daff51fa533c37769ae40561554addc37ab1a4
Benjamin Rutledge <ben.d.rutledge@gmail.com>
2021-02-02 13:33:14 +0000

    completes utils functions

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,7 +1,9 @@
 const { expect } = require("chai");
 const { it } = require('mocha');
 const {
-  formatLine
+  formatLine,
+  convertSemicolonToNewLine,
+  formatsData
 } = require("../utils");
 
 describe('formatLine', () => {
@@ -21,4 +23,40 @@ describe('formatLine', () => {
         expect(output).to.equal('Stuart Adams, Stuart.Adams@eu.jll.com');
     });
 });
+
+describe('convertSemicolonToNewLine', () => {
+    it('should return a string', () => {
+        const output = convertSemicolonToNewLine("");
+        expect(output).to.be.a('string');
+    });
+    it('should return string on 1 line when passed string with no semicolon', () => {
+        const output = convertSemicolonToNewLine("hello");
+        expect(output).to.equal("hello");
+    });
+    it('should return string on 2 lines when passed string with a semicolon', () => {
+        const input = "hello;goodbye"
+        const output = convertSemicolonToNewLine(input);
+        expect(output).to.equal("hello\ngoodbye");
+    });
+});
+
+describe('formatsData', () => {
+    it('should return one formatted contact on one line', () => {
+        const input = 'Adams, Stuart <Stuart.Adams@eu.jll.com>'
+        const output = formatsData(input);
+        expect(output).to.equal('Stuart Adams, Stuart.Adams@eu.jll.com');
+    });
+    it('should return two formatted contacts on two lines', () => {
+        const input = 'Adams, Stuart <Stuart.Adams@eu.jll.com>; Afsar, Zeeshan <Zeeshan.Afsar@eu.jll.com>'
+        const output = formatsData(input);
+        expect(output).to.equal('Stuart Adams, Stuart.Adams@eu.jll.com\nZeeshan Afsar, Zeeshan.Afsar@eu.jll.com');
+    });
+    it('should return any mutiple of contacts on an equal number of lines', () => {
+        const input = 'Adams, Stuart <Stuart.Adams@eu.jll.com>; Afsar, Zeeshan <Zeeshan.Afsar@eu.jll.com>; Agnew, Jane <Jane.Agnew@eu.jll.com>; Ambrose, Laura <Laura.Ambrose@eu.jll.com>; Anderson, Tamryn <Tamryn.Anderson@eu.jll.com>; Andrew, Rachel <Rachel.Andrew@eu.jll.com>; Arnold, Christine <Christine.Arnold@eu.jll.com>; Asher, Yafeth <Yafeth.Asher@eu.jll.com>'
+        const output = formatsData(input);
+        expect(output).to.equal('Stuart Adams, Stuart.Adams@eu.jll.com\nZeeshan Afsar, Zeeshan.Afsar@eu.jll.com\nJane Agnew, Jane.Agnew@eu.jll.com\nLaura Ambrose, Laura.Ambrose@eu.jll.com\nTamryn Anderson, Tamryn.Anderson@eu.jll.com\nRachel Andrew, Rachel.Andrew@eu.jll.com\nChristine Arnold, Christine.Arnold@eu.jll.com\nYafeth Asher, Yafeth.Asher@eu.jll.com');
+    });
+    
+});
+
 
--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,5 @@
-exports.formatLine = string => {
-    const dataArray = string.split(" ");
+const formatLine = str => {
+    const dataArray = str.split(" ");
     const firstName = dataArray[1];
     const surname = dataArray[0];
     const fullName = `${firstName} ${surname}`;
@@ -10,4 +10,36 @@ exports.formatLine = string => {
     const emailAddress = emailAddressArr.join("");
     return `${fullName} ${emailAddress}`;
 
+};
+
+const convertSemicolonToNewLine = str => {
+    const arr = str.split("");
+    const filteredArr = arr.map((char)=>{
+        if (char === ";") {
+            return "\n"
+        } else return char
+    })
+    return filteredArr.join("");
+};
+
+const formatsData = str => {
+    const contacts = str.split(";")
+    const formattedLines = contacts.map((contact)=> {
+        if (contact[0] === " "){
+             const charArr = contact.split("");
+             charArr.shift();
+             return formatLine(charArr.join(""));
+        } else {
+            return formatLine(contact);
+        }
+    })
+    const result = formattedLines.join("\n");
+    console.log(result);
+    return result;
+};
+
+module.exports = {
+    formatLine,
+    convertSemicolonToNewLine,
+    formatsData
 }
